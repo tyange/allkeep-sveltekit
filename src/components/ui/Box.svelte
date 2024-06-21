8e3a69ec1e10d280b134b934bdb5ad21c889d117
tyange <usun16@gmail.com>
2024-06-21 23:58:19 +0900

    Box Component

---
+++ src/components/ui/Box.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface BoxProps {
+		children?: Snippet;
+	}
+
+	let { children }: BoxProps = $props();
+</script>
+
+<div
+	class="flex w-fit items-center justify-center rounded-lg border border-gray-300 bg-white bg-opacity-90 p-10 text-black shadow-md"
+>
+	{#if !children}
+		coming soon
+	{:else}
+		{@render children()}
+	{/if}
+</div>
---
+++ src/stories/Box.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+import Box from '../components/ui/Box.svelte';
+
+const meta = {
+	title: 'Components/Box',
+	component: Box,
+	parameters: {
+		layout: 'centered'
+	},
+	tags: ['autodocs']
+} satisfies Meta<Box>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
