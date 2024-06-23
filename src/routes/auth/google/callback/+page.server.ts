04f418983adf703d0bef63ef24a4b18fa4717034
tyange <usun16@gmail.com>
2024-06-23 15:46:53 +0900

    구글 로그인 로직을 client에서 server로 옮김

---
+++ src/routes/auth/google/callback/+page.server.ts
@@ -0,0 +1,42 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ url, fetch, cookies }) => {
+	const code = url.searchParams.get('code');
+
+	let isSuccess = false;
+	let isPending = false;
+
+	if (!code) {
+		throw new Error('Invalid google login code.');
+	}
+
+	isPending = true;
+
+	try {
+		const res = await fetch('http://localhost:8080/auth/google', {
+			method: 'POST',
+			body: JSON.stringify({ code })
+		});
+		const data = await res.json();
+
+		cookies.set('session', data.token, {
+			path: '/',
+			sameSite: 'strict',
+			maxAge: 60 * 60 * 2
+		});
+
+		isSuccess = true;
+		isPending = false;
+	} catch (err) {
+		console.log(err);
+		isSuccess = false;
+		isPending = false;
+	}
+
+	return {
+		auth: {
+			isSuccess,
+			isPending
+		}
+	};
+};
--- src/routes/auth/google/callback/+page.svelte
+++ src/routes/auth/google/callback/+page.svelte
@@ -1,45 +1,23 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import axios from 'axios';
+	import type { PageData } from './$types';
 
 	import Box from '@/components/ui/Box.svelte';
-	import { goto } from '$app/navigation';
 
-	let isSuccess = $state(false);
-	let isError = $state(false);
+	export let data: PageData;
 
-	const sendCodeToServer = async () => {
-		const location = document.location;
-		const searchParams = new URLSearchParams(location.search);
-		const code = searchParams.get('code');
-
-		if (!code) {
-			throw new Error('Invalid google login code.');
-		}
-
-		try {
-			const data = await axios.post('http://localhost:8080/auth/google', { code });
-
-			console.log(data);
-			isSuccess = true;
-			// goto('');
-		} catch (err) {
-			console.log(err);
-			isError = true;
-		}
-	};
-
-	onMount(async () => {
-		sendCodeToServer();
-	});
+	// if (document) {
+	// 	console.log(document.cookie);
+	// }
 </script>
 
 <Box>
-	{#if isSuccess}
+	{#if data.auth.isSuccess && !data.auth.isPending}
 		<span>구글 로그인에 성공했습니다.</span>
-	{:else if isError}
+	{/if}
+	{#if !data.auth.isSuccess && !data.auth.isPending}
 		<span>구글 로그인에 실패했습니다.</span>
-	{:else}
+	{/if}
+	{#if data.auth.isPending}
 		<span>구글 로그인을 시도하는 중입니다...</span>
 	{/if}
 </Box>
